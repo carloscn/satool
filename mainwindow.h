8e92158401850b91ef71e60f05ad2bad21007a29
carloscn <lifi.mlt@gmail.com>
2021-06-27 19:11:55 +0800

    [bugfix] qftp timeout lead to crash
    qftp timeout lead to crash
    
    closed #5

--- mainwindow.h
+++ mainwindow.h
@@ -117,6 +117,8 @@ private:
     bool voltage5v;
     QButtonGroup *voltageGroup;
     NetClientThread *socket;
+    QTimer *timerAntiDiscon;
+    QString curFtpPath;
 
 private:
 
@@ -131,6 +133,8 @@ private:
     void qwtPlotFft( int, double *, int);
     void drawData(int pos);
     void get_ini_file_data();
+    bool deleteFtpDirectory(QFtp *ftp, const QString &path);
+    void ftpCd(QString path);
 
 protected:
     void keyPressEvent(QKeyEvent *event);
@@ -169,6 +173,7 @@ private slots:
     void on_actionDownloadFile_triggered();
     void on_actionconfig_triggered();
     void on_pushButton_merge_downloads_clicked();
+    void on_timerAntiConn_timeout();
 };
 
 #endif // MAINWINDOW_H
